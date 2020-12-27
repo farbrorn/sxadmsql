0250bf860f5f5304193a2e8be971c23b6731a0c5
ulf <ulf@LAPTOP-RNVUDMQV.lan>
2020-12-27 08:56:59 +0100

    fix

--- Scripts/sxas/SxAsOverforOrder.sql
+++ Scripts/sxas/SxAsOverforOrder.sql
@@ -1,4 +1,3 @@
---drop FUNCTION sxasOverforOrder2Offert(in_orderlista integer[])
 
 CREATE OR REPLACE FUNCTION sxasOverforOrder2Offert(in_orderlista integer[], out out_offertnr integer, out out_viktkg integer, out out_antalkolli integer)  AS
 $BODY$
@@ -84,12 +83,3 @@ $BODY$
 
  
  
--- select * from sxasOverforOrder2Offert(array [13882])
-
- --update sxasfakt.order1 set status='Samfak' where ordernr=13918
---select * from offert2 where offertnr=24138
-
---select * from sxasfakt.order2 where ordernr in (13882, 13816, 10560, 10116)
-
-
-
