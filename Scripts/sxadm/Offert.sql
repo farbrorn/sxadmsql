5fea7806aa876dbc2f668371b3097e2e54d32ad3
ulf <ulf@LAPTOP-RNVUDMQV.lan>
2020-12-27 07:52:27 +0100

    xsasOverforOrder2Offert

---
+++ Scripts/sxadm/Offert.sql
@@ -0,0 +1,33 @@
+
+CREATE OR REPLACE FUNCTION offert1add(
+    in_anvandare character varying,
+    in_lagernr integer,
+    in_kundnr character varying,
+    in_marke character varying)
+  RETURNS integer AS
+$BODY$
+declare
+	this_offertnr integer;
+begin
+	
+	if not exists (select 1 from lagerid where lagerid.lagernr=in_lagernr) then raise exception 'Lagernummer % saknas', in_lagernr; end if;
+	if not exists (select 1 from saljare where forkortning=in_anvandare) then raise exception 'Användare % saknas', in_anvandare; end if;
+	if not exists (select 1 from kund where nummer=in_kundnr) then raise exception 'Kundnummer % saknas', in_kundnr; end if;
+	
+	select offertnr into this_offertnr from faktdat;
+	update faktdat set offertnr = offertnr+1;
+	insert into offert1 (offertnr, namn, adr1, adr2, adr3, levadr1, levadr2, levadr3, 
+		saljare, referens, kundnr, marke, datum, moms, status, ktid, bonus, faktor, levdat, 
+		levvillkor, mottagarfrakt, fraktkundnr, fraktbolag, fraktfrigrans, lagernr,
+		skrivejpris
+	)
+		select this_offertnr, namn, adr1, adr2, adr3, lnamn, ladr2, ladr3,
+		saljare, ref, nummer, in_marke, current_date, case when momsfri <> 0 then 1 else 0 end, 'Sparad', ktid, bonus, faktor, null,
+		levvillkor, mottagarfrakt, fraktkundnr, fraktbolag, fraktfrigrans, in_lagernr,
+		0
+		from kund where nummer=in_kundnr;
+	return this_offertnr;
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
---
+++ Scripts/sxas/SxAsOverforOrder.sql
@@ -0,0 +1,83 @@
+CREATE OR REPLACE FUNCTION sxasOverforOrder2Offert(in_orderlista integer[], out offertnr integer, out out_viktkg integer, out out_kolli integer)  AS
+$BODY$
+declare
+	this_offertnr integer;
+	this_antalkolli integer;
+	this_viktkg integer;
+	this_meddelande varchar;
+	this_iserror boolean;
+begin
+	--Kolla efter felaktiga ordrar samt räkna vikt 
+	select 
+	string_agg( ' Order ' || o.ordernr ||
+	case when ejsamfak+stjarnrader+saknadeartiklar+kreditrader+saknarinprisrader > 0 then 
+		case when ejsamfak > 0 then  ' - Ej på samfakt eller saknas. ' else '' end ||
+		case when  stjarnrader > 0 then  ' - Innehåller icke tillåtna *-rader. ' else '' end ||
+		case when  saknadeartiklar > 0 then  ' - Innehåller rader som saknas i artikleregistret. ' else '' end ||
+		case when  kreditrader > 0 then  ' - Innehåller kreditrader. ' else '' end ||
+		case when  saknarinprisrader > 0 then  ' - Saknar inköpspris i artikelregistret. ' else '' end 
+	else ' - OK' end 
+	, '<br>') as meddelande 
+	, sum(ejsamfak+stjarnrader+saknadeartiklar+kreditrader+saknarinprisrader) > 0 as iserror 
+	, sum(antalkolli) as antalkolli 
+	, sum(viktkg) as viktkg 
+	into this_meddelande, this_iserror, this_antalkolli, this_viktkg
+	from (
+		select olist.ordernr 
+		, case when o1.status='Samfak' then 0 else 1 end as ejsamfak
+		, sum(case when o2.artnr like '*%' then 1 else 0 end)  as stjarnrader
+		, sum(case when a.nummer is null then 1 else 0 end)  as saknadeartiklar
+		, sum(case when o2.lev < 0 then 1 else 0 end)  as kreditrader
+		, sum(case when a.getinnetto =0 then 1 else 0 end)  as saknarinprisrader
+		, max(kol.antalkolli) as antalkolli
+		, max(kol.viktkg) as viktkg
+		from
+		(select unnest(array [1,21146,2]) as ordernr) olist 
+		left outer join sxasfakt.order1 o1 on o1.ordernr=olist.ordernr 
+		left outer join sxasfakt.order2 o2 on o1.ordernr=o2.ordernr and trim(coalesce(o2.artnr,'')) <> ''
+		left outer join artikel a on a.nummer=o2.artnr
+		left outer join (select wmsordernr, count(*) as antalkolli, sum(viktkg) as viktkg from sxfakt.wmskollin group by wmsordernr) kol on kol.wmsordernr= 'AS-' || o1.ordernr
+		group by olist.ordernr, o1.ordernr
+	) o; 
+	
+	-- Fel på någon order
+	if this_iserror then raise exception '%', this_meddelande; end if;
+
+
+	--Skapa offert	
+	select offert1add('00',0,'00478','') into this_offertnr ;
+
+	insert into offert2 (offertnr, pos, prisnr, artnr, namn, levnr, best, rab, lev, text, pris, summa, konto, netto, enh)
+	select this_offertnr, row_number() over (), 1, o2.artnr, a.namn, a.lev, o2.lev, 0, o2.lev, ''
+	, round(case when a.getinnetto <= 0 then 
+		case when o2.pris >= 0 then 
+			o2.pris * (1-coalesce(o2.rab,0)/100) * valuta.kurs 
+			else utpris end 
+	else a.getinnetto + greatest(0,(o2.pris * (1-coalesce(o2.rab,0)/100) * valuta.kurs) - a.getinnetto) * 0.44 end::numeric ,2) as pristillas
+	, 0, '', a.getinnetto, a.enhet
+	from
+	(select unnest(in_orderlista) as ordernr) olist 
+	join sxasfakt.order1 o1 on o1.ordernr=olist.ordernr 
+	join sxasfakt.order2 o2 on o1.ordernr=o2.ordernr and trim(coalesce(o2.artnr,'')) <> ''
+	join artikel a on a.nummer=o2.artnr
+	join valuta on valuta.valuta='NOK'
+	where o2.artnr not like '*%' and o2.lev > 0;
+	
+	update offert2 set summa=round((lev*pris*(1-rab/100))::numeric,2) where offertnr=this_offertnr;
+
+	select this_offertnr as offertnr, this_viktkg as viktkg, this_antalkolli as antalkolli;
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
+
+ 
+ 
+-- select sxasOverforOrder2Offert(array [13902, 13880])
+
+--select * from offert2 where offertnr=24138
+
+--select * from sxasfakt.order2 where ordernr in (13902, 13880)
+
+
+
